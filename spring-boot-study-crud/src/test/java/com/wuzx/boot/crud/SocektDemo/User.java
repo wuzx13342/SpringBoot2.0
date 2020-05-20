4f76bd798b0554b11e15755353d177b80e5acc36
邬志祥 <wuzhixiang@51xf.cn>
2020-05-20 10:11:23 +0800

    netty测试

--- spring-boot-study-crud/src/test/java/com/wuzx/boot/crud/SocektDemo/User.java
+++ spring-boot-study-crud/src/test/java/com/wuzx/boot/crud/SocektDemo/User.java
@@ -5,8 +5,9 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-    public class User implements Serializable {
+public class User implements Serializable {
 
+    private static final long serialVersionUID = -7328074295442506967L;
     private String name;
 
 }
